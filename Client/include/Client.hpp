12c887e93dc4ad3e341e4056111091698030a898
Ruben Rodriguez <rodriguez.ruben96@gmail.com>
2020-03-30 17:47:15 -0500

    Finishing UI

--- Client/include/Client.hpp
+++ Client/include/Client.hpp
@@ -1,40 +1,66 @@
 #include <iostream>
+#include <gtkmm.h>
 #include <string>
 #include <vector>
 #include <deque>
 #include <thread>
 
-#include "asio.hpp"
-#include "Player.hpp"
-#include "Connection.hpp"
-#include "chat_message.hpp"
+//#include "asio.hpp"
+//#include "Player.hpp"
+//#include "Connection.hpp"
+//#include "chat_message.hpp"
 
 class CLIENT{
     private:
         bool inGame;
-        PLAYER player;
-        int id;
-        CONNECTION connection;
-        asio::io_context io_context;
-        tcp::resolver resolver(io_context);
-        auto endpoints;
+        //PLAYER player;
+        //CONNECTION connection;
+        Glib::RefPtr<Gtk::Application> app;
+        Glib::RefPtr<Gtk::Builder> builder;
+        Gtk::Window *window;
     public:
         CLIENT(){
-            inGame = false;
-        }
-        void create_player(std::string name, int id){
-            player = PLAYER(name, id);
-        }
-        void start_game(){
-            inGame = true;
-        }
-        void end_game(){
-            inGame = false;
-        }
-        void resolve_endpoints(){
+            app = Gtk::Application::create("PokerProject.Team11");
+            builder = Gtk::Builder::create_from_file("glade/MainWindow.glade");
+
+            window = nullptr;
+            Gtk::Stack *stack = nullptr;
+            
+            builder->get_widget("Window", window);
+            builder->get_widget("Stack", stack);
+
+            Gtk::Entry *entry_name = nullptr;
+            Gtk::Entry *entry_ip = nullptr;
+            Gtk::Entry *entry_port = nullptr;
+            Gtk::Button *btn_join = nullptr;
+            Gtk::Button *btn_spectate = nullptr;
+
+            Gtk::Label *lbl_player_one = nullptr;
+            Gtk::Button *btn_leave_game = nullptr;
+
+            builder->get_widget("entry_name", entry_name);
+            builder->get_widget("entry_ip", entry_ip);
+            builder->get_widget("entry_port", entry_port);
+            builder->get_widget("btn_join", btn_join);
+            builder->get_widget("btn_spectate", btn_spectate);
+
+            btn_join->signal_clicked().connect(sigc::bind(sigc::ptr_fun(&enter_game), stack));
+
+
+            builder->get_widget("lbl_player_one", lbl_player_one);
+            builder->get_widget("btn_leave_game", btn_leave_game);
+
+            stack->set_visible_child("login_screen");
 
         }
-        void connect(){
-            connection = CONNECTION(io_context, endpoints);
+        Glib::RefPtr<Gtk::Application> get_app(){
+            return this->app;
+        }
+        Gtk::Window * get_window(){
+            return this->window;
+        }
+        static void enter_game(void *stack){
+            Gtk::Stack *tempStack = static_cast<Gtk::Stack *>(stack);
+            tempStack->set_visible_child("game_screen");
         }
 };
--- Client/src/chat_client.cpp
+++ Client/src/chat_client.cpp
@@ -1,41 +1,15 @@
 #include <gtkmm.h>
 #include <iostream>
 
-// GtkWidget *entry_ip;
-// GtkWidget *entry_port;
-// GtkWidget *btn_join;
-// GtkWidget *btn_spectate;
-// GtkWidget *window2;
-// GtkWidget *window;
-Gtk::Window *window = nullptr;
-Gtk::Window *window2 = nullptr;
-
-static void enter_game(Glib::RefPtr<Gtk::Application> app){
-   window->hide();
-   app->run(*window2);
-}
+#include "../include/Client.hpp"
 
 int main(int argc, char *argv[]){
 
-   auto app = Gtk::Application::create(argc, argv, "PokerProject.Team11");
+   CLIENT client;
 
-   Glib::RefPtr<Gtk::Builder> builder = Gtk::Builder::create_from_file("glade/MainWindow.glade");
-   builder->get_widget("MainWindow", window);
-   builder->get_widget("GameWindow", window2);
-   Gtk::Entry *entry_ip = nullptr;
-   builder->get_widget("entry_ip", entry_ip);
-   Gtk::Entry *entry_port = nullptr;
-   builder->get_widget("entry_port", entry_port);
-   Gtk::Button *btn_join = nullptr;
-   builder->get_widget("btn_join", btn_join);
-   Gtk::Button *btn_spectate = nullptr;
-   builder->get_widget("btn_spectate", btn_spectate);
+   client.get_app()->run(*client.get_window());
 
-   //btn_join->signal_clicked().connect(sigc::bind(sigc::ptr_fun(&enter_game), app));
-
-   app->run(*window);
-
-   delete window;
+   delete client.get_window();
 
    return 0;
 
